5f29b435490522022138212d6535647ec80ade4d
sashank-gl <glsashank365@gmail.com>
2024-02-10 23:43:05 +0530

    Removed all candidate recruiter logic

--- api/index.js
+++ api/index.js
@@ -1,28 +1,18 @@
 const express = require("express");
 
 // Middlewares
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 dotenv.config();
-const dbUri = process.env.DB_URI;
 
 // Starting Express App
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
 
 app.use(cors());
 
-// Database Connection
-mongoose
-  .connect(dbUri)
-  .then(() => {
-    console.log("Connected to ElevateX");
-  })
-  .catch((error) => {
-    console.error("MongoDB Connection Error: ", error);
-  });
+// Routes
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,9 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Login from "./auth_pages/Login";
-import Register from "./auth_pages/Register";
+import React from 'react'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
